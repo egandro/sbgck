6b25e59949dfebf4347e1c2f4eca308165480437
Harald Fielker <harald.fielker@gmail.com>
2021-03-03 21:41:28 +0100

    .

--- @sbgck/desktop/src/api/audio.implementation.ts
+++ @sbgck/desktop/src/api/audio.implementation.ts
@@ -1,10 +1,13 @@
 import { Audio } from '@sbgck/os-api';
 
+const addon = require('../../build/Release/addon');
+
 const player = require('play-sound')();
 
 export class AudioImplementation implements Audio {
 	playMp3(fileName: string): boolean {
-		// blocking player
+		console.log("AudioImplementation says greetings from C++ ", addon.hello());
+		// non blocking player
 		player.play(fileName, (err: Error)=>{
 			if (err) throw err
 		});
